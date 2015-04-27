dab0087b60f57a4df6fd91ba09f3f5faa4578002
Qingqing Li <qqli@bu.edu>
2015-04-27 14:38:11 -0400

    group name

--- network/js/main.js
+++ network/js/main.js
@@ -301,7 +301,9 @@ function configSigmaElements(config) {
     $GP.bg2 = $(sigInst._core.domElements.bg2);
     var a = [],
         b,x=1;
-		for (b in sigInst.clusters) a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> Group ' + (x++) + ' (' + sigInst.clusters[b].length + ' members)</a></div>');
+		for (b in sigInst.clusters) {
+            //console.log(sigInst.clusters[b][0].attr)
+            a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> ' + sigInst.clusters[b][0].attr+ ' (' + sigInst.clusters[b].length + ' members)</a></div>');}
     //a.sort();
     $GP.cluster.content(a.join(""));
     b = {
